dc7f734f9501e361483560a11eb952e83a63a1da
GsLogimaker <gslogimaker@gmail.com>
2024-04-25 15:01:14 -0500

    Impl default for ecs_type_hooks_t

--- flecs-sys/src/lib.rs
+++ flecs-sys/src/lib.rs
@@ -7,7 +7,7 @@
 #![allow(deref_nullptr)]
 #![allow(improper_ctypes)]
 
-use std::mem::MaybeUninit;
+use std::{ffi::c_void, mem::MaybeUninit};
 
 pub mod bindings;
 pub use bindings::*;
@@ -16,42 +16,47 @@ pub use bindings::*;
 //
 impl Default for ecs_entity_desc_t {
 	fn default() -> Self {
-		let desc: ecs_entity_desc_t = unsafe { MaybeUninit::zeroed().assume_init() };
-		desc
+		unsafe { MaybeUninit::zeroed().assume_init() }
 	}
 }
 
 impl Default for ecs_filter_desc_t {
 	fn default() -> Self {
-		let desc: ecs_filter_desc_t = unsafe { MaybeUninit::zeroed().assume_init() };
-		desc
+		unsafe { MaybeUninit::zeroed().assume_init() }
 	}
 }
 
 impl Default for ecs_observer_desc_t {
 	fn default() -> Self {
-		let desc: ecs_observer_desc_t = unsafe { MaybeUninit::zeroed().assume_init() };
-		desc
+		unsafe { MaybeUninit::zeroed().assume_init() }
 	}
 }
 
 impl Default for ecs_query_desc_t {
 	fn default() -> Self {
-		let desc: ecs_query_desc_t = unsafe { MaybeUninit::zeroed().assume_init() };
-		desc
+		unsafe { MaybeUninit::zeroed().assume_init() }
 	}
 }
 
 impl Default for ecs_system_desc_t {
 	fn default() -> Self {
-		let desc: ecs_system_desc_t = unsafe { MaybeUninit::zeroed().assume_init() };
-		desc
+		unsafe { MaybeUninit::zeroed().assume_init() }
 	}
 }
 
 impl Default for ecs_term_t {
 	fn default() -> Self {
-		let desc: ecs_term_t = unsafe { MaybeUninit::zeroed().assume_init() };
-		desc
+		unsafe { MaybeUninit::zeroed().assume_init() }
+	}
+}
+
+impl ecs_type_hooks_t {
+	fn set_binding_ctx<T>(&mut self, data: T) {
+		let ptr:*mut T = Box::leak(Box::new(data));
+		self.binding_ctx = ptr.cast::<c_void>()
+	}
+} impl Default for ecs_type_hooks_t {
+	fn default() -> Self {
+		unsafe { MaybeUninit::zeroed().assume_init() }
 	}
 }
